046411ac891d0f49e493faef352ca2f3ba692feb
Luis Adiel Martínez <luisadiel99@gmail.com>
2023-02-17 19:14:32 -0600

    Add lowercase display names to scalar types

--- src/main/java/org/automatas/engine/ScalarType.java
+++ src/main/java/org/automatas/engine/ScalarType.java
@@ -1,9 +1,20 @@
 package org.automatas.engine;
 
 public enum ScalarType {
-    ARRAY,
-    BOOLEAN,
-    FLOAT,
-    INTEGER,
-    STRING,
+    ARRAY("array"),
+    BOOLEAN("bool"),
+    FLOAT("float"),
+    INTEGER("int"),
+    STRING("string");
+
+    private String displayName;
+
+    ScalarType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
